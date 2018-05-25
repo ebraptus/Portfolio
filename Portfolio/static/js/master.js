e92efdd559dcfe6de4795804ff2cc87287be14b2
John D <themaxtwo2@yahoo.com>
2018-05-25 08:26:26 -0400

    Better Mobile Support
    Added events to check if the user is swiping on the side bar and if so not to scroll the page
    
    Added events to see if the user swipes outside of sidebar or touches outside of sidebar and making the sidebar close.

--- Portfolio/static/js/master.js
+++ Portfolio/static/js/master.js
@@ -32,3 +32,29 @@ $("#mobileNavToggle").click(function(event){
 
 	$("body").toggleClass("showMobileNav");
 });
+
+$("nav.mobile").bind('touchmove', function(e){
+	e.preventDefault();
+})
+
+$("#mobileNavToggle").bind('touchmove', function(e){
+	e.preventDefault();
+})
+
+$('body,html').click(function(e){
+	var menu = $("nav.mobile");
+	var toggle = $("#mobileNavToggle");
+	
+	if (!menu.is(e.target) && !toggle.is(e.target) && menu.has(e.target).length === 0 && toggle.has(e.target).length === 0) {
+		$("body").removeClass('showMobileNav');
+	}
+});
+
+ $('body,html').bind("touchmove", function(e){
+	var menu = $("nav.mobile");
+	var toggle = $("#mobileNavToggle");
+	
+	if (!menu.is(e.target) && !toggle.is(e.target) && menu.has(e.target).length === 0 && toggle.has(e.target).length === 0) {
+		$("body").removeClass('showMobileNav');
+	}
+});
