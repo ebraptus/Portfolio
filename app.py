5c0d93fc4d7a793f5d34384276c80a0aa3b340c9
Ebraptus <4898712+ebraptus@users.noreply.github.com>
2018-06-03 18:13:58 -0400

    Updated structure and added section for internships

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from CDN import cdn
 
 app = Flask(__name__)
 
-app.config["SERVER_NAME"] = "johndeshano.me"	
+app.config["SERVER_NAME"] = "johndeshano.me"
 
 app.register_blueprint(portfolio)
 app.register_blueprint(cdn, subdomain="cdn")
 
 if __name__ == "__main__":
-	app.config["SERVER_NAME"] = "localhost:2020"	
+	app.config["SERVER_NAME"] = "localhost:2020"
 	app.run(host="0.0.0.0", port="2020", debug=True)
