9fb493ad928f9ba33b6db4daa07b1b3341dab163
ebraptus <4898712+ebraptus@users.noreply.github.com>
2018-06-17 12:23:31 -0400

    Move some stuff around, fixed some links

--- Portfolio/__init__.py
+++ Portfolio/__init__.py
@@ -2,6 +2,11 @@ from flask import Blueprint, render_template
 
 portfolio = Blueprint("portfolio", __name__, template_folder="templates", static_folder="static", static_url_path="/static/portfolio")
 
+
+@portfolio.route("/<path:invalid_path>/")
+def handle_unmatchable(*args, **kwargs):
+    return render_template('404.html', title="404"), 404
+
 @portfolio.errorhandler(404)
 def page_not_found(error):
 	return render_template('404.html', title="404"), 404
