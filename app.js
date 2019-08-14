5f5c0a1d77439dcecaa6b44b08a432c654100d58
Ebraptus <4898712+ebraptus@users.noreply.github.com>
2019-08-14 09:43:45 -0400

    Website not enforces SSL in production

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const lessMiddleware = require("less-middleware");
 const compression = require("compression");
+const enforce  = require("express-sslify");
 const mainRouter = require("./router");
 
 const app = express();
+const env = process.env.NODE_ENV || "dev";
 
 // * Sets up the view engine and views directory
 app.set("views", __dirname + "/templates");
@@ -11,6 +13,8 @@ app.set("view engine", "pug");
 
 // * Set up middleware
 app.use(compression()); // * Compresses all content
+if(env == "production") // * Enforce SSL in production
+  app.use(enforce.HTTPS({ trustProtoHeader: true })) 
 
 // * Serves static content with LESS Middleware
 app.use(lessMiddleware(__dirname + "/static"));
